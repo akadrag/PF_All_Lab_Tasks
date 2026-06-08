6af974509885f77e3ea337a5f7a86ca4450afb80
akadrag <aghahussainabbas54@gmail.com>
2026-06-08 15:50:59 +0500

    Added new files

--- PF_Lab_Week_11/task1.py
+++ PF_Lab_Week_11/task1.py
@@ -1,1 +1,5 @@
 # Task 1
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j, end ="\t")
+    print()
---
+++ PF_Lab_Week_11/task2.py
@@ -0,0 +1,7 @@
+# Task 2
+for i in range(1,6):
+    for j in range(50-i):
+        print("", end ="")
+    for j in range(2*i-1):
+        print("*", end ="")
+    print()
